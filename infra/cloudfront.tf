7adb9d52169ba53577454dc05dacd425a51fd058
kalabtech <235978118+kalabtech@users.noreply.github.com>
2026-03-30 08:28:26 +0100

    feat(cloudfront): add distribution with oac

---
+++ infra/cloudfront.tf
@@ -0,0 +1,46 @@
+# TODO: Distribution does not have logging enabled and distribution does not utilize a WAF.
+#trivy:ignore:AVD-AWS-0010
+#trivy:ignore:AVD-AWS-0011
+resource "aws_cloudfront_distribution" "this" {
+  origin {
+    domain_name              = module.s3.bucket.bucket_regional_domain_name
+    origin_id                = "s3-static-website"
+    origin_access_control_id = aws_cloudfront_origin_access_control.this.id
+  }
+
+  enabled             = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    target_origin_id       = "s3-static-website"
+    viewer_protocol_policy = "redirect-to-https"
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+
+    # NOTE: AWS managed CachingOptimized policy
+    cache_policy_id = "658327ea-f89d-4fab-a63d-7e88639e58f6"
+  }
+
+  price_class = var.price_class
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_cloudfront_origin_access_control" "this" {
+  name                              = "static-website-oac"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
